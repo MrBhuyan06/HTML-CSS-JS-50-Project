daec96e8967a06d98f33db70ad020542b089550e
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-05-18 00:08:44 +0530

    Added the 6 project scroll Animation

--- 07-ScrollAnimation/script.js
+++ 07-ScrollAnimation/script.js
@@ -1,1 +1,20 @@
+const box = document.querySelectorAll(".box");
 
+window.addEventListener("scroll", scrolling);
+
+scrolling();
+function scrolling() {
+  //get the trigger point basically the point where the animation start
+  //   console.log(window.innerHeight);
+  const triggerPoint = (window.innerHeight * 4) / 5;
+  //   console.log(triggerPoint);
+  box.forEach((box) => {
+    const boxPostionTop = box.getBoundingClientRect().top;
+    // console.log(boxPostionTop);
+    if (triggerPoint > boxPostionTop) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
