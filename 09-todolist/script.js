4138add4b485dc397556371b4949a6bb0196d18b
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-24 15:54:10 +0530

    added the todo list

--- 09-todolist/script.js
+++ 09-todolist/script.js
@@ -1,1 +1,28 @@
+window.addEventListener("load", () => {
+  let form = document.querySelector("#input-from");
+  let inputfrom = document.querySelector("#input-text");
+  let list_ele = document.querySelector("#list-task");
+  //   console.log(form);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let taskvalue = inputfrom.value;
 
+    const html = `
+    <div id="task">
+    <div id="contain">
+     
+      <input type="text' value="${taskvalue}"  />
+    </div>
+    <div>
+      <button>Edit ${taskvalue}</button>
+      <button>Delete</button>
+    </div>
+  </div>
+
+    `;
+    console.log(html);
+    let ele = list_ele.insertAdjacentHTML("afterbegin", html);
+    console.log(ele);
+  });
+});
+// let btn = document.querySelector("#btn");
