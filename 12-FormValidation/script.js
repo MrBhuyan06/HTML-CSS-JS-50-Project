442ed458cf59ffdac91cdc80cf694eb764a6a04c
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-29 23:19:47 +0530

    Added the html file

--- 12-FormValidation/script.js
+++ 12-FormValidation/script.js
@@ -1,1 +1,22 @@
-
+const number = document.getElementById("number");
+const password = document.getElementById("password");
+const name = document.getElementById("name");
+const btn = document.getElementById("btn");
+const demo = document.getElementById("demo");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(number.value, password.value, name.value);
+});
+// demo.addEventListener("click", () => {
+//   console.log("click for demo 1");
+// });
+// demo.addEventListener("click", () => {
+//   // console.log('click for demo 1');
+//   console.log("demo is click 2 function");
+// });
+// function demo() {
+//   console.log("demo is trigger");
+// }
+// function demo2() {
+//   console.log("demo is trigger");
+// }
