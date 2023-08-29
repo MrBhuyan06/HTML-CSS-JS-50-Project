23af192cc641916be7eb3571bbe337e678c3d378
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-29 23:29:30 +0530

    Added the js

--- 12-FormValidation/script.js
+++ 12-FormValidation/script.js
@@ -2,11 +2,12 @@ const number = document.getElementById("number");
 const password = document.getElementById("password");
 const name = document.getElementById("name");
 const btn = document.getElementById("btn");
-const demo = document.getElementById("demo");
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(number.value, password.value, name.value);
-});
+const form = document.getElementById("form");
+// const demo = document.getElementById("demo");
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log(number.value, password.value, name.value);
+// });
 // demo.addEventListener("click", () => {
 //   console.log("click for demo 1");
 // });
@@ -20,3 +21,13 @@ btn.addEventListener("click", (e) => {
 // function demo2() {
 //   console.log("demo is trigger");
 // }
+// function formHandler(e) {
+//   e.preventDefault();
+//   console.log(e);
+//   //   console.log(e.target.name);
+//   //   console.log("hello");
+// }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target);
+});
