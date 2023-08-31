264856578457a7b8be06898b45f05e97097975d4
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-31 23:29:25 +0530

    Added the error contain

--- 13-TodoList/script.js
+++ 13-TodoList/script.js
@@ -1,1 +1,13 @@
-
+const main = document.getElementById("main-container");
+const error = document.getElementById("error");
+const todoValue = document.getElementById("todoname");
+const todoList = [];
+const submit = document.getElementById("sub-btn");
+submit.addEventListener("click", () => {
+  console.log(todoValue.value);
+  if (!todoValue.value) {
+    error.style.display = "block";
+  } else {
+    error.style.display = "none";
+  }
+});
